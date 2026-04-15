54c14052dfe82261e72118080a52c275d890545d
Jos <b1t@mailbox.org>
2026-04-15 08:59:15 +0800

    Initial Commit

---
+++ trackpad_control/trackpad_controlApp.swift
@@ -0,0 +1,17 @@
+//
+//  trackpad_controlApp.swift
+//  trackpad_control
+//
+//  Created by Jos on 15/4/26.
+//
+
+import SwiftUI
+
+@main
+struct trackpad_controlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ trackpad_controlTests/trackpad_controlTests.swift
@@ -0,0 +1,19 @@
+//
+//  trackpad_controlTests.swift
+//  trackpad_controlTests
+//
+//  Created by Jos on 15/4/26.
+//
+
+import Testing
+@testable import trackpad_control
+
+struct trackpad_controlTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
